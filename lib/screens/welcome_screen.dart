2ed7bff5cd4907d9ae4454e8da3a34a80452d0d5
iamuv2000 <yuvraj.singh2018@vitstudent.ac.in>
2020-03-30 21:17:21 +0530

    Init

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+import 'registration_screen.dart';
+import 'registration_screen.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import '../components/RoundedButton.dart';
+
+class WelcomeScreen extends StatefulWidget {
+
+  static String id = 'welcome_screen';
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin{
+  AnimationController controller;
+  Animation animation;
+
+  @override
+
+  void initState(){
+    super.initState();
+    controller= AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+      upperBound: 1
+    );
+    animation = ColorTween(
+      begin: Colors.blueGrey,
+      end: Colors.white
+    ).animate(controller);
+    controller.forward();
+    animation.addStatusListener((status){
+    });
+
+    controller.addListener((){
+      setState(() {
+
+      });
+    });
+  }
+
+
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: animation.value,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Hero(
+                  tag:'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 60,
+                  ),
+                ),
+                TypewriterAnimatedTextKit(
+                  text: ['Flash Chat'],
+                  textStyle: TextStyle(
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            RoundedButton(
+              onPressed: (){
+                Navigator.pushNamed(context, LoginScreen.id);
+              },
+              color: Colors.lightBlueAccent,
+              text: 'Log in'
+            ),
+            RoundedButton(
+              onPressed: () {
+                //Go to registration screen.
+                Navigator.pushNamed(context, RegistrationScreen.id);
+              },
+              text: 'Register',
+              color:  Colors.blueAccent
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
